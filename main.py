ab4e253a3d35c9c6f2f4ba71255dcbab2d0d6dbb
K3K5LP <44317845+K3K5LP@users.noreply.github.com>
2025-04-30 22:45:33 +0200

    fehler beheben

--- main.py
+++ main.py
@@ -3,70 +3,77 @@ import datetime
 import numpy as np
 import matplotlib.pyplot as plt
 
+
 def calculate(raw_data):
     scale = [val[0] for val in raw_data]
     rel = calc_relative(raw_data)
-    poisson = calc_poisson(raw_data, rel)
+    poisson, info = calc_poisson(raw_data, rel)
 
-    return scale, rel, poisson
+    return scale, rel, poisson, info
+
 
 def calc_relative(raw_data):
-    data = [val[1] for val in raw_data]
+    _data = [val[1] for val in raw_data]
     count = 0
-    for i in data:
-        count += i
+    for _ in _data:
+        count += _
     relative = []
-    for i in data:
-        relative.append(i/count)
+    for _ in _data:
+        relative.append(_/count)
     return relative
+
 
 def calc_poisson(raw_data, rel):
     repetitions = [val[0] for val in raw_data]
     expected_value = 0
-    for i in repetitions:
-        expected_value += i*rel[i]
+    n = 0
+    for _ in repetitions:
+        expected_value += _*rel[_]
+        n += raw_data[_][1]
     poisson = []
     for k in repetitions:
         temp = (expected_value**k)/math.factorial(k)*math.exp(-expected_value)
         poisson.append(temp)
-    return poisson
+        info = [expected_value, (expected_value/n)**0.5, n]
+    return poisson, info
 
-def search_index(data, target):
-    for i in data:
-        if i[0] == target:
+
+def search_index(_data, target):
+    for _ in _data:
+        if _[0] == target:
             return i
 
-def plot(_data):
-    x_values, RelHäuf, poisson = _data
-    #Values = [[0, 11], [1, 80], [2, 74], [3, 71], [4, 48], [5, 28], [6, 13], [7, 9], [8, 2]]
-    #RelHäuf = [1,2,3,4,5,6,7,8,9]
-    #poisson = [1,2,3,4,5,6,7,8,9]
 
+def plot(_data):
+    x_values, rel_occurrence, poisson, junk = _data
 
     bar_width = 1
     x_indexes = np.arange(len(x_values))
-    plt.figure(figsize=(8, 4))
-    plt.bar(x_indexes, RelHäuf, width=bar_width, label='relative Häufigkeit', hatch='||', alpha=1, edgecolor='black',
-            facecolor='none', linewidth=0.8)
-    plt.bar(x_indexes, poisson, width=bar_width, label='Poissonwahrscheinlichkeit', hatch='//', alpha=1,
+    plt.figure(figsize=(7, 7))
+    plt.bar(x_indexes, rel_occurrence, width=bar_width, label='relative Häufigkeit', hatch='||', alpha=1,
+            edgecolor='black', facecolor='none', linewidth=0.8)
+    plt.bar(x_indexes, poisson, width=bar_width, label='Poisson-Wahrscheinlichkeit', hatch='//', alpha=1,
             edgecolor='black', facecolor='none', linewidth=0.8)
 
     plt.xlabel('Ereignisse')
     plt.ylabel('Wahrscheinlichkeit')
-    plt.title('Vergleich Poissonwahrscheinlichkeit mit relativer Häufigkeit', loc='left',y=1.05)
+    plt.title('Vergleich Poisson-Wahrscheinlichkeit mit relativer Häufigkeit', loc='left', y=1.04)
+    plt.gca().yaxis.set_minor_locator(plt.MultipleLocator(0.0125))
+    plt.yticks(np.arange(0, max(max(rel_occurrence), max(poisson)), 0.025))
     plt.xticks(x_indexes, x_values)
     plt.legend()
-    plt.tick_params(axis='both', direction='in')
+    plt.tick_params(axis='both', which="both", direction='in', top=True, right=True)
 
-    plt.figtext(1, 1.05, f"Marius Trabert, {datetime.datetime.now().strftime("%d. %B %Y")}", ha='right', va='top', transform=plt.gca().transAxes, fontsize=10)
-    plt.figtext(0,  1.05, "[Histogramm 1]", fontsize=10, ha="left",va="top",transform=plt.gca().transAxes)
+    plt.figtext(1, 1.035, f"Marius Trabert, {datetime.datetime.now().strftime('%d. %B %Y')}", ha='right', va='top',
+                transform=plt.gca().transAxes, fontsize=10)
     plt.xlim(-0.5, 8.5)
     plt.show()
 
 
 if __name__ == '__main__':
+    legend = ["Ereignisse:\t\t\t\t\t", "Relative Häufigkeit:\t\t", "Poisson Wahrscheinlichkeit:\t", "µ,σ,n:\t\t\t\t\t\t"]
     data = [[0, 23], [1, 83], [2, 71], [3, 74], [4, 45], [5, 19], [6, 16], [7, 3], [8, 2]]
     calc = calculate(data)
-    for i in calc:
-        print(i)
+    for i in range(len(calc)):
+        print(legend[i], calc[i])
     plot(calc)
